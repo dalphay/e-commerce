1e5548fd594aea8c8c19436512a7ffecf2ed495f
jackobdiallo@gmail.com <jackobdiallo@gmail.com>
2018-04-19 15:34:02 +0200

    projet e-commerce mis à jour

--- src/app/produit-fiche/produit-fiche.component.ts
+++ src/app/produit-fiche/produit-fiche.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-produit-fiche',
   templateUrl: './produit-fiche.component.html',
-  styleUrls: ['./produit-fiche.component.css']
+  styleUrls: ['./produit-fiche.component.scss']
 })
 export class ProduitFicheComponent implements OnInit {
 
--- src/app/produit-list/produit-list.component.ts
+++ src/app/produit-list/produit-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-produit-list',
   templateUrl: './produit-list.component.html',
-  styleUrls: ['./produit-list.component.css']
+  styleUrls: ['./produit-list.component.scss']
 })
 export class ProduitListComponent implements OnInit {
 
